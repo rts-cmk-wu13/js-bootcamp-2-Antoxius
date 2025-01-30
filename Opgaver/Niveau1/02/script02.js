5f6259caeab2ec7c33a63acfbf027eb375fb4377
Xenius Tolderlund <Xeniusawbt@gmail.com>
2025-01-30 11:25:34 +0100

    Opgave 2 løst

--- Opgaver/Niveau1/02/script02.js
+++ Opgaver/Niveau1/02/script02.js
@@ -1,6 +1,12 @@
 function beregnForhold(a, b) {
   return a / b;
 }
+function beregnForhold2(a, b) {
+  if (b == 0) return null
+  return a / b;
+}
 
 let resultat = beregnForhold(4, 3);
+let resultat2 = beregnForhold2(12, 3);
 console.log(resultat); // Udskriver 1.3333333333333333
+console.log(resultat2); // Udskriver 1.3333333333333333
